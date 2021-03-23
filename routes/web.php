f5a0847b6c4c439a0d55627b7c463d0c6d9a4ad7
JavierFadel <javierfadel1208@gmail.com>
2021-03-24 06:56:27 +0700

    v0.1

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,17 @@ use App\Http\Controllers\ScheduleController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteServicePr ovider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+*/	
 
 Route::get('/', function () {
     return view('auth.login');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
-	// Route for dashboard.
+	// Route for dashboard.	
 	Route::get('/dashboard', function(){
 		return view('dashboard');
 	})->name('dashboard');
